3ace9717d934eee9dda549b6eaa8f2a5a1687634
shamrice <shamrice@gmail.com>
2019-09-06 18:22:20 -0400

    #91 username field lengthened. Email field was already set long enough. Added some max length attributes to some inputs

--- src/main/resources/schema/create_database_mysql.sql
+++ src/main/resources/schema/create_database_mysql.sql
@@ -33,7 +33,20 @@ CREATE TABLE owner (
 );
 
 
-commit;
+CREATE TABLE discapp_user (
+  id int(20) NOT NULL AUTO_INCREMENT,
+  username varchar(50) UNIQUE NOT NULL,
+  password varchar(255) NOT NULL,
+  email varchar(255) UNIQUE NOT NULL,
+  show_email bool NOT NULL DEFAULT FALSE,
+  owner_id int(20),
+  enabled bool NOT NULL DEFAULT TRUE,
+  is_admin bool NOT NULL DEFAULT FALSE,
+  create_dt TIMESTAMP DEFAULT NOW(),
+  mod_dt TIMESTAMP DEFAULT NOW(),
+  PRIMARY KEY (id),
+  FOREIGN KEY (owner_id) REFERENCES owner(id)
+);
 
 
 CREATE TABLE application (
--- src/main/resources/schema/create_database_postgresql.sql
+++ src/main/resources/schema/create_database_postgresql.sql
@@ -32,7 +32,7 @@ CREATE TABLE owner (
 
 CREATE TABLE discapp_user (
     id serial NOT NULL,
-    username varchar(20) UNIQUE NOT NULL,
+    username varchar(50) UNIQUE NOT NULL,
     password varchar(255) NOT NULL,
     email varchar(255) UNIQUE NOT NULL,
     show_email bool NOT NULL DEFAULT FALSE,
