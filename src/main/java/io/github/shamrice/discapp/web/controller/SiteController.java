f3320dda610be9995b3d3a997e7946a0057da6b6
shamrice <shamrice@gmail.com>
2019-11-03 01:36:25 -0400

    #34 created base home page place holder

--- src/main/java/io/github/shamrice/discapp/web/controller/SiteController.java
+++ src/main/java/io/github/shamrice/discapp/web/controller/SiteController.java
@@ -6,8 +6,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -17,6 +20,11 @@ public class SiteController {
 
     @Autowired
     private ConfigurationService configurationService;
+
+    @GetMapping("/")
+    public ModelAndView getIndexView(Model model) {
+        return new ModelAndView("index", "model", model);
+    }
 
     @RequestMapping(value = "/robots.txt", method = RequestMethod.GET, produces = "text/plain")
     public String getRobotsTxt(Model model, HttpServletResponse response) {
