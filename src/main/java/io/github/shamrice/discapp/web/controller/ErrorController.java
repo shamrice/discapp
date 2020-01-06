2c3bfbde8cea6e0aa70967db1306f06c02755dcd
shamrice <shamrice@gmail.com>
2020-01-06 03:28:42 -0500

    #18 error page no longer shows detailed error and now has a return to homepage button added

--- src/main/java/io/github/shamrice/discapp/web/controller/ErrorController.java
+++ src/main/java/io/github/shamrice/discapp/web/controller/ErrorController.java
@@ -5,7 +5,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -23,33 +25,39 @@ public class ErrorController implements org.springframework.boot.web.servlet.err
     private static final String ERROR_EXCEPTION_ATTRIBUTE_NAME = "javax.servlet.error.exception";
     private static final String ATTEMPTED_URI_ATTRIBUTE_NAME = "javax.servlet.error.request_uri";
 
-    @RequestMapping(value = ErrorUrl.CONTROLLER_DIRECTORY_URL, produces = "text/html")
+    @RequestMapping(value = ErrorUrl.CONTROLLER_DIRECTORY_URL, produces = "text/html", method = RequestMethod.GET)
     public ModelAndView getErrorView(HttpServletRequest request, Model model) {
 
         Integer statusCode = (Integer) request.getAttribute(STATUS_CODE_ATTRIBUTE_NAME);
         Exception exception = (Exception) request.getAttribute(ERROR_EXCEPTION_ATTRIBUTE_NAME);
         String attemptedUri = (String) request.getAttribute(ATTEMPTED_URI_ATTRIBUTE_NAME);
 
-
-        String errorText = "";
-        if (exception != null) {
-            errorText = exception.getMessage();
+        String errorLogMessage = "";
+        if (exception != null && exception.getMessage() != null) {
+            errorLogMessage = exception.getMessage();
         }
 
+        log.error("Error: status code: " + statusCode + " : " + errorLogMessage + " :: attempted uri: "
+                + attemptedUri, exception);
 
         if (statusCode != null && statusCode.equals(HttpStatus.FORBIDDEN.value())) {
-            log.info("Status code 403 " + errorText, exception);
+            log.info("Status code 403 " + errorLogMessage, exception);
             return new ModelAndView("redirect:/login?relogin=true&" + request.getQueryString());
         }
 
-        log.error("Error: status code: " + statusCode + " : " + errorText + " :: attempted uri: "
-                + attemptedUri, exception);
+        String errorText = "An error has occurred. Please try again.";
 
         model.addAttribute(ERROR_STATUS_CODE, statusCode);
         model.addAttribute(ERROR_TEXT, errorText);
 
         return new ModelAndView("error/error", "model", model);
 
+    }
+
+    @PostMapping(ErrorUrl.CONTROLLER_DIRECTORY_URL)
+    public ModelAndView postErrorController() {
+        //always just redirect to homepage.
+        return new ModelAndView("redirect:/");
     }
 
     @RequestMapping(value = ERROR_NOT_FOUND)
@@ -65,7 +73,7 @@ public class ErrorController implements org.springframework.boot.web.servlet.err
     }
 
     public String getErrorPath() {
-        return "/error";
+        return ErrorUrl.CONTROLLER_DIRECTORY_URL;
     }
 
 }
